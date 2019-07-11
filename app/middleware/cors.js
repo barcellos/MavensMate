fc450dc9c8028f7c3a44bd52a5c5971a1313ef69
Joe Ferraro <ferraro.joseph@gmail.com>
2019-07-11 06:39:28 -0400

    Update cors.js

--- app/middleware/cors.js
+++ app/middleware/cors.js
@@ -1,5 +1,5 @@
 module.exports = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:56248');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,PUT,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', ['Content-Type', 'X-Requested-With', 'MavensMate-Editor-Agent']);
   next();
